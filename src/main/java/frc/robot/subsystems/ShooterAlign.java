1408c204a6d58ba9820e20f971620a04b2b72f5f
Stratiz <36082806+Stratiz@users.noreply.github.com>
2021-06-13 16:55:34 -0500

    Fix equation

--- src/main/java/frc/robot/subsystems/ShooterAlign.java
+++ src/main/java/frc/robot/subsystems/ShooterAlign.java
@@ -75,33 +75,33 @@ public class ShooterAlign extends SubsystemBase {
     }
     private double[] vectorLerp(double[] a , double[] b ,double t) {
         double bSubA[] = {b[0]-a[0],b[1]-a[1]};
-        double aPlusSub[] = {a[0]+bSubA[0],a[1]+bSubA[1]};
-        double result[] = {aPlusSub[0]*t,aPlusSub[1]*t};
+        double aMultSub[] = {bSubA[0]*t,bSubA[1]*t};
+        double result[] = {a[0]+aMultSub[0],a[1]+aMultSub[1]}; 
         return result;
-        
+            
     }
     private double[] cubicBezier(double t, double[] p0, double[] p1, double[] p2, double[] p3) {
         double l1[] = vectorLerp(p0, p1, t);
-	    double l2[] = vectorLerp(p1, p2, t);
-	    double l3[] = vectorLerp(p2, p3, t);
-    	double a[] = vectorLerp(l1, l2, t);
-    	double b[] = vectorLerp(l2, l3, t);
-    	double cubic[] = vectorLerp(a, b, t);
-	    return cubic;
+        double l2[] = vectorLerp(p1, p2, t);
+        double l3[] = vectorLerp(p2, p3, t);
+        double a[] = vectorLerp(l1, l2, t);
+        double b[] = vectorLerp(l2, l3, t);
+        double cubic[] = vectorLerp(a, b, t);
+        return cubic;
     }
     public double getTargetPosition(double distance) {
         System.out.println("HELLO " + distance);
         if (distance >= 48 && distance <= 147.2) {
-            double whole = (147.2 - distance);
+            double whole = (147.2 - 48);
             double p0[] = {48,0.1};
             double p1[] = {103.5,4};
-            double p2[] = {125.7,13.5};
+            double p2[] = {125.7,12};
             double p3[] = {142.7,0};
 
             double result[] = cubicBezier((distance-48)/whole, p0, p1, p2, p3);
-            System.out.println(result[0]);
-            System.out.println(result[1]);
-            System.out.println(distance);
+            System.out.println((distance-48)/whole);
+            System.out.println(result[0] + ", " + result[1]);
+            return result[1];
         }
         return 0.0;
     }
